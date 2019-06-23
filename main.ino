f3c57a4a84a76b636e128e759014deb78c95a661
alainlou <az2lou@uwaterloo.ca>
2019-06-22 21:47:19 -0400

    some fixes

--- main.ino
+++ main.ino
@@ -12,17 +12,19 @@ FPS_GT511C3 fps(3, 1); // (Arduino SS_RX = pin 4, Arduino SS_TX = pin 5)
 const char * ssid = "eduroam";                  // The name of the Wi-Fi network that will be created
 const char * username = "az2lou@uwaterloo.ca";  // The password required to connect to it, leave blank for an open network
 const char * password = "";
-unsigned long start;
+unsigned long startTime;
 unsigned long onTime;
+unsigned long refTime;
+unsigned long offRefTime;
 WiFiClient client;
 HTTPClient http;
 
 void setup() {
-  pinMode(2, INPUT);  
+  pinMode(2, INPUT);
   pinMode(0, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, INPUT);
-  
+
   Serial.begin(115200);
   delay(1000);
 
@@ -45,9 +47,9 @@ void setup() {
   wifi_station_set_enterprise_identity((uint8*)username, strlen(username));
   wifi_station_set_enterprise_username((uint8*)username, strlen(username));
   wifi_station_set_enterprise_password((uint8*)password, strlen(password));
-  
+
   wifi_station_connect();
-  
+
   Serial.print("Status: ");
   Serial.println(wifi_station_get_connect_status());
 
@@ -62,34 +64,29 @@ void setup() {
 
 void loop() {
   if (digitalRead(2) == LOW) {
-    start = millis();
-    onTime += (start-refTime);
-    refTime = start;
+    startTime = millis();
+    onTime += (startTime - refTime);
+    refTime = startTime;
   }
-  if(digitalRead(5) == HIGH) {
+  if (digitalRead(5) == HIGH) {
     digitalWrite(4, HIGH);
     Serial.println("Request sent!");
-    try {
-      http.begin(client, "http://alainlou.api.stdlib.com/enghack@dev/verify/?name=Test&verified=false");
-      int httpCode = http.GET();
-      Serial.println(httpCode);
-      Serial.println(http.getString());  
-      delay(250);
-    }
-    catch {
-      Serial.println("Failed, please try again.");
-    }
+    http.begin(client, "http://alainlou.api.stdlib.com/enghack@dev/verify/?name=Test&verified=false");
+    int httpCode = http.GET();
+    Serial.println(httpCode);
+    Serial.println(http.getString());
+    delay(250);
   } else {
     digitalWrite(4, LOW);
-  } 
+  }
 
-  if(WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED) {
     digitalWrite(0, HIGH);
   } else {
     digitalWrite(0, LOW);
   }
-  if (offRefTime>80000) {
-    if (onTime/offRefTime >= 0.75) {
+  if (offRefTime > 8000) {
+    if (onTime / offRefTime >= 0.75) {
       Serial.println("Busy mode detected");
     }
     else {
